4612280726e63d7bfd36831b5f4c1c9706c2666f
White Anonym <white_anonym@SeungJins-MacBook-Pro.local>
2012-07-29 12:26:00 +0900

    Initial Commit

---
+++ dummy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dummy
+//
+//  Created by SeungJin Lee on 12. 7. 29..
+//  Copyright (c) 2012년 white_anonym@hanmail.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
